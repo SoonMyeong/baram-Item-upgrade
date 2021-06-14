aebcb415e25b96519cb39919d2cfdebc05958a26
soonmyeong <smkwon@neonexsoft.com>
2021-06-14 23:13:10 +0900

    리팩토링 - 중복 메소드 제거 - 서비스 수정 완료 TODO - 단위테스트 작성

--- src/main/java/com/soonworld/upgrade/service/UpgradeItemService.java
+++ src/main/java/com/soonworld/upgrade/service/UpgradeItemService.java
@@ -5,118 +5,124 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UpgradeItemService {
 
-    public Mono<UpgradeItem> upgradeItem(UpgradeItem item) throws NullPointerException{
-        try {
-            if(!item.isUpgradeItemUsed() && !item.isUpgradeDoubleItemUsed()) { //강화비급 x, 강화촉진제 x
-                return checkUpgradeItem(item.getCurrentItemLevel(),false,false);
-            }else if(item.isUpgradeItemUsed() && !item.isUpgradeDoubleItemUsed()) { //강화비급 o, 강화촉진제 x
-                return checkUpgradeItem(item.getCurrentItemLevel(),false,false);
-            }else if(!item.isUpgradeItemUsed() && item.isUpgradeDoubleItemUsed()){ //강화비급 x , 강화촉진제 o
-                return checkUpgradeItem(item.getCurrentItemLevel(),false,false);
-            }else {  //강화비급 o , 강화촉진제 o
-                return checkUpgradeItem(item.getCurrentItemLevel(),false,false);
-            }
-        }catch(NullPointerException e){
-            throw new NullPointerException(e.getMessage());
+    /**
+     * 강화 결과값을 구하고, 강화비급과 강화촉진제의 사용유무에 따른 값을 부여 해 주는 메소드
+     *
+     * @param item
+     * @return
+     * @throws NullPointerException
+     */
+    public Mono<UpgradeItem> upgradeItem(UpgradeItem item) throws NullPointerException {
+        //강화 결과
+        int result = (int) (Math.random() * 100) + 1;
+        //성공 확률
+        int percent = currentLevelToPercent(Optional.of(item.getCurrentItemLevel()).orElse(0));
+        //강화 비급 확률 3%
+        int upgradeB = 3;
+        //강화 촉진제 사용 결과 확률
+        int upgraceC = (int) (Math.random() * 100) + 1;
+
+        if (!item.isUpgradeItemUsed() && !item.isUpgradeDoubleItemUsed()) { //강화비급 x, 강화촉진제 x
+            return upgrade(result,percent,0,0);
+        } else if (item.isUpgradeItemUsed() && !item.isUpgradeDoubleItemUsed()) { //강화비급 o, 강화촉진제 x
+            return upgrade(result,percent,upgradeB,0);
+        } else if (!item.isUpgradeItemUsed() && item.isUpgradeDoubleItemUsed()) { //강화비급 x , 강화촉진제 o
+            return upgrade(result,percent,0,upgraceC);
+        } else {  //강화비급 o , 강화촉진제 o
+            return upgrade(result,percent,upgradeB,upgraceC);
         }
     }
 
+    /**
+     * 아래 파라미터를 입력 받아 확률에 따라 강화 성공과 실패 그리고 유지를 결정 짓고 <br>
+     *  Vo를 만들어 리턴 하는 메소드
+     *
+     * @param result  강솨 성공 결과
+     * @param percent  현재 장비 레벨에서의 강화 성공 확률
+     * @param upgradeB 강화비급 (3%)
+     * @param upgradeC 강화촉진제 (50%)
+     * @return
+     */
+    public Mono<UpgradeItem> upgrade(int result, int percent, int upgradeB, int upgradeC) {
+        int resultItemAddLevel = 0;
+        boolean upgradeResult = false;
 
-//    /**
-//     * 아이템 사용 유무(caseNumber)에 따른 메소드 라우팅 해주는 메소드
-//     *
-//     */
-//    public void routeUpgradeMethod(UpgradeItem item, boolean isUpgradeB , boolean isUpgradeC){
-//        int number = (int) (Math.random()*100)+1; //강화 결과
-//
-//        switch(item.getCurrentItemLevel()){
-//            case 1:
-//            case 2:
-//                if(caseNumber==1) itemCase.upgradeOne(item,100,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,100,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,100,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,100,number);
-//                break;
-//            case 3:
-//                if(caseNumber==1) itemCase.upgradeOne(item,90,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,90,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,90,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,90,number);
-//                break;
-//            case 4:
-//                if(caseNumber==1) itemCase.upgradeOne(item,80,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,80,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,80,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,80,number);
-//                break;
-//            case 5:
-//                if(caseNumber==1) itemCase.upgradeOne(item,70,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,70,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,70,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,70,number);
-//                break;
-//            case 6:
-//                if(caseNumber==1) itemCase.upgradeOne(item,60,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,60,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,60,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,60,number);
-//                break;
-//            case 7:
-//                if(caseNumber==1) itemCase.upgradeOne(item,50,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,50,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,50,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,50,number);
-//                break;
-//            case 8 :
-//                if(caseNumber==1) itemCase.upgradeOne(item,40,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,40,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,40,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,40,number);
-//                break;
-//            case 9:
-//                if(caseNumber==1) itemCase.upgradeOne(item,30,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,30,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,30,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,30,number);
-//                break;
-//            case 10:
-//                if(caseNumber==1) itemCase.upgradeOne(item,20,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,20,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,20,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,20,number);
-//                break;
-//            case 11:
-//                if(caseNumber==1) itemCase.upgradeOne(item,15,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,15,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,15,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,15,number);
-//                break;
-//            case 12:
-//                if(caseNumber==1) itemCase.upgradeOne(item,12,number);
-//                else if(caseNumber==2) itemCase.upgradeTwo(item,12,number);
-//                else if(caseNumber==3) itemCase.upgradeThree(item,12,number);
-//                else if(caseNumber==4) itemCase.upgradeFour(item,12,number);
-//                break;
-//
-//            default: System.out.println("upgrade Item Routing  number Exception!");
-//        }
-//    }
+        if(result<=percent+upgradeB) {
+            upgradeResult = true;
+            if(upgradeC>50) {
+                resultItemAddLevel = 2;
+            }else {
+                resultItemAddLevel = 1;
+            }
+        }else {
+            //강화 유지 확률 30%
+            int addNum = (int) (Math.random()*100)+1;
+            if(addNum>30){
+                resultItemAddLevel = -1;
+            }
+        }
 
-    public Mono<UpgradeItem> checkUpgradeItem(int currentLevel, boolean isUpgradeB, boolean isUpgradeC) {
-        int number = (int) (Math.random()*100)+1; //강화 결과
-        int percent = 100;
-        //TODO
-        return upgrade(percent,number);
-    }
-
-    public Mono<UpgradeItem> upgrade(int percent, int num) {
-        //TODO
-        return Mono.just(UpgradeItem.builder().build());
+        return Mono.just(UpgradeItem.builder()
+                .upgradeResult(upgradeResult)
+                .resultItemAddLevel(resultItemAddLevel)
+                .build());
     }
 
 
+
+    /**
+     * 입력받은 장비의 현재 레벨을 강화 성공 확률로 리턴
+     *
+     * @param currentLevel
+     * @return result
+     */
+    public int currentLevelToPercent(int currentLevel) {
+        int result = 0;
+        switch (currentLevel) {
+            case 1:
+            case 2:
+                result = 100;
+                break;
+            case 3:
+                result = 90;
+                break;
+            case 4:
+                result = 80;
+                break;
+            case 5:
+                result = 70;
+                break;
+            case 6:
+                result = 60;
+                break;
+            case 7:
+                result = 50;
+                break;
+            case 8:
+                result = 40;
+                break;
+            case 9:
+                result = 30;
+                break;
+            case 10:
+                result = 20;
+                break;
+            case 11:
+                result = 15;
+                break;
+            case 12:
+                result = 12;
+                break;
+
+            default:
+                result = 1;
+        }
+        return result;
+    }
 }
